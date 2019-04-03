b6e5cd5041ecea119f3d692fec7f85270c7f1491
kaushal <kaushal_1995@yahoo.ca>
2019-04-02 23:34:38 -0400

    working on player header

--- Projects/RainDrops/RainDrops/RainDrops/Player.h
+++ Projects/RainDrops/RainDrops/RainDrops/Player.h
@@ -1,8 +1,18 @@
 #pragma once
-class Player
+
+#include "Sprite.h"
+#include <SFML\System.hpp>
+
+class Player : public Sprite
 {
+	DECLARE_DYNAMIC_DERIVED_CLASS(Player, Sprite)
+
 public:
-	Player();
-	~Player();
+	void initialize() override;
+	virtual void update(float deltaTime) override;
+	virtual void load(XMLElement* element) override;
+
+	void writeCreate(RakNet::BitStream & bs) const override;
+	void readCreate(RakNet::BitStream & bs) override;
 };
 
